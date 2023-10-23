aea4adb50a46e03c460c622aeada420b024994ef
shoaibfarooq3 <shoaibfarooq3@gmail.com>
2023-10-23 13:03:19 +0500

    Updated Code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,60 @@
 import { createRequire } from "module";
-
 const require = createRequire(import.meta.url);
 const pkg = require("../package.json");
+const redis = require("redis");
+let redisClient;
+// var host = '127.0.0.1';
+var host = "localhost";
+// const branchModel = require("./models/branchModel.js");
+require("dotenv").config();
+// let client = redis.createClient({
+//   socket: {
+//     host: host,
+//     port: 6379,
+//     connect_timeout: 5000,
+//   }
+// });
 
-/**
- * @summary Import and call this function to add this plugin to your API.
- * @param {Object} app The ReactionAPI instance
- * @returns {undefined}
- */
+const io = require("socket.io")(6397, {
+  cors: {
+    origin: "*",
+  },
+  //  path:'/location'
+});
+//var host = '13.52.55.53';
+
+
+(async () => {
+  console.log(`Connecting to Redis server at ${host}`);
+  redisClient = redis.createClient({
+    socket: {
+      host: host,
+      port: 6379,
+      connect_timeout: 5000,
+    },
+  });
+  redisClient.on("error", (error) => console.error(`Error ${host} : ${error}`));
+  await redisClient.connect();
+})();
+function trackingStartUp(context) {
+  console.log("here startup");
+  // client.on('error', (err) => {
+  //   console.log(`Error ${err}`);
+  // });
+  // console.log("branchModel", branchModel);
+  // console.log("client ", client);
+  console.log("redisClient", redisClient);
+  // io.on("connection", async (socket) => {
+  //   console.log("socket", socket);
+  // })
+}
 export default async function register(app) {
   await app.registerPlugin({
     label: pkg.label,
     name: pkg.name,
-    version: pkg.version
+    version: pkg.version,
+    functionsByType: {
+      startup: [trackingStartUp]
+    },
   });
 }
---
+++ src/models/branchModel.js
@@ -0,0 +1,17 @@
+// import { createRequire } from "module";
+// const require = createRequire(import.meta.url);
+// var mongoose = require('mongoose');
+
+// var Schema = mongoose.Schema;
+// var branch = new Schema({
+//     "branchname": {
+//         type: String,
+//         required: true
+//     },
+//     "branchaddress": String,
+//     "branchphonenumber": String,
+//     "branchCity": String,
+//     "branchSector": String,
+//     "branchTiming": String,
+// });
+// module.exports = mongoose.model('BranchData', branch);
